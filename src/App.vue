45086d2911c1f799f9760e236af7526de7ab7c72
Blackdigits <kelvinjunilson@gmail.com>
2019-04-18 12:57:42 +0700

    Created template for website

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Checkout></Checkout>
+  </div>
+</template>
+
+<script>
+import Checkout from "./components/Checkout.vue";
+
+export default {
+  name: "app",
+  components: {
+    Checkout
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
+</style>
---
+++ src/components/Checkout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="checkout-box">
+    <div class="form-box">
+      <div class="progress-box">
+        <div class="progress-item">
+          <div class="circle">1</div>
+          <span>Delivery</span>
+        </div>
+        <div class="progress-item-separator">></div>
+        <div class="progress-item">
+          <div class="circle">2</div>
+          <span>Payment</span>
+        </div>
+        <div class="progress-item-separator">></div>
+        <div class="progress-item">
+          <div class="circle">3</div>
+          <span>Finish</span>
+        </div>
+      </div>
+      <h2>Form</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus">
+@import '../css/checkout.styl';
+</style>
